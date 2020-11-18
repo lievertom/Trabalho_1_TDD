c4f176ca7d049bd73f89253e5b4430dfa178fb0e
Lieverton <lievertom@gmail.com>
2020-11-18 10:51:19 -0300

    create LerArquivoEntradaTest

---
+++ Parser/src/tst/LerArquivoEntradaTest.java
@@ -0,0 +1,21 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LerArquivoEntradaTest {
+
+	@Before
+	public void setup() {
+		parser = new Parser;
+	}
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
