9add64ec16969b9eac6ee67e9f6304e5ccd135d4
Lieverton <lievertom@gmail.com>
2020-11-19 14:16:43 -0300

    create DefinirDelimitadorDeCampoTest

---
+++ Parser/src/tst/DefinirDelimitadorDeCampoTest.java
@@ -0,0 +1,25 @@
+package tst;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import app.Parser;
+
+public class DefinirDelimitadorDeCampoTest {
+
+	private Parser parser;
+
+	@Before
+	public void setup() {
+		parser = new Parser();
+	}
+	
+	
+	@Test
+	public void testDefinirDelimitadorDeCampo() throws DelimitadorInvalidoException {
+		parser.setDelimiter(';');
+		assertEquals(';', parser.getDelimiter());
+	}
+}
